af824d3c47e6a0384a74b9a7cacb0112fe5d44af
appsdev3 <appsdev3@matt-dev.(none)>
2011-09-08 12:30:46 -0500

    bumped version after adding django-jenkins

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ TEST_REQUIREMENTS = [
 def do_setup():
     setup(
         name="django-wizard",
-        version='0.2.1',
+        version='0.2.2',
         author="Matthew J. Morrison",
         author_email="mattjmorrison@mattjmorrison.com",
         description="A wizard that helps to control page flow.",
